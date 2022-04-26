a734ef7f56d270a15cfde7379100a626667961a6
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-04-26 23:30:49 +0100

    barcode

--- barcode.py
+++ barcode.py
@@ -1,6 +1,18 @@
 #import the module Qrcode
 import qrcode
-#declare a variable to hold the qr function make()
-img = qrcode.make("https://github.com/Niruifedi")
+
+#creating a QRcode object
+obj_qr = qrcode.QRCode(
+    version = 1,
+    error_correction = qrcode.constants.ERROR_CORRECT_L,
+    box_size = 10,
+    border = 4,
+)
+
+#using the add_data() function
+obj_qr.add_data("https://github.com/Niruifedi")
+
+#using the make_image() function
+qr_img = obj_qr.make_image(fill_color = "red", back_color = "black")
 #save the file in jpg format for easy access
-img.save("gitprofile.jpg")
+qr_img.save("gitprofile.jpg")
